dd613c96539f711ea98db274c9b089ba03712421
wajoasetbmd-maker <wajoasetbmd@gmail.com>
2026-07-29 15:19:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aset-wajo-v17-5';
-const SHELL=['./','./index.html','./manifest.webmanifest','./assets/logo-wajo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/v17-5.css','./assets/v17-5.js'];
+const CACHE='aset-wajo-v17-6';
+const SHELL=['./','./index.html','./manifest.webmanifest','./assets/logo-wajo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/v17-6.css','./assets/v17-6.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
