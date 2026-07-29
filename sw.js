3daf8cf021a875eddbb128ef2476c59bfbd0d2b5
wajoasetbmd-maker <wajoasetbmd@gmail.com>
2026-07-29 13:40:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aset-wajo-v17-4';
-const SHELL=['./','./index.html','./manifest.webmanifest','./assets/logo-wajo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/v17-4.css','./assets/v17-4.js'];
+const CACHE='aset-wajo-v17-5';
+const SHELL=['./','./index.html','./manifest.webmanifest','./assets/logo-wajo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/v17-5.css','./assets/v17-5.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
@@ -10,8 +10,5 @@ self.addEventListener('fetch',event=>{
     event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy)).catch(()=>{});return res}).catch(()=>caches.match('./index.html')));
     return;
   }
-  event.respondWith(caches.match(req).then(cached=>{
-    const network=fetch(req).then(res=>{if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});}return res;}).catch(()=>cached);
-    return cached||network;
-  }));
+  event.respondWith(fetch(req).then(res=>{if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});}return res}).catch(()=>caches.match(req)));
 });
